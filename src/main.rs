22e182905e0da84b703f71d89dfb9d95c7fdff26
Adam P <adam.porbanderwalla@gmail.com>
2026-02-16 16:13:19 -0500

    feat: added testing for questions, and it seems all handling works well

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 mod question;
 
+use crate::question::{AnswerType, QuestionType, ask_question};
+
 fn main() {
-    println!("Hello, world!");
+    let question = "How was your day? ";
+    let qt = QuestionType::Float;
+    let ans = ask_question(question, qt).unwrap(); 
+    match ans {
+        AnswerType::Float(t) => println!("You said: {t}"),
+        _ => println!("Unexpected Type")
+    }; 
 }
+
