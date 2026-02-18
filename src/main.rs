f9ee96e21cfe6bf9d9c8e85381f4d7537c314849
Adam P <adam.porbanderwalla@gmail.com>
2026-02-17 20:12:12 -0500

    feat: started planning for how I am going to handle the file management protion of the app

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 mod question;
-
+mod files;
 use crate::question::{AnswerType, QuestionType, ask_question};
 
 fn main() {
-    let question = "How was your day? ";
-    let qt = QuestionType::Float;
-    let ans = ask_question(question, qt).unwrap(); 
-    match ans {
-        AnswerType::Float(t) => println!("You said: {t}"),
-        _ => println!("Unexpected Type")
-    }; 
 }
 
