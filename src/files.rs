b5a90b8bf79d65da5db2e3795a69e3fe03987ad5
Adam P <adam.porbanderwalla@gmail.com>
2026-02-19 21:28:45 -0500

    fix: changed the name of the folder from accontability to data

--- src/files.rs
+++ src/files.rs
@@ -1,6 +1,6 @@
 use serde; 
 use std::fs::{self, create_dir, File};
-use std::path::{Path}; 
+use std::path::{Path, PathBuf}; 
 use chrono;
 use std::io;
 use crate::question::Question; 
@@ -8,17 +8,35 @@ use std::ffi::OsStr;
 //Ex. path: &Path = Path&::new("/Users/adamporbanderwalla/Desktop")
 
 fn create_new_entry() -> (){
-    
+         
 }
 
-fn create_all_necessary_files() -> (){
-    
+fn create_all_necessary_files(path: &Path) -> (){
+    // Given a path we want to create all of the necessary files for  
+}
+
+fn check_data_exists(path: &Path) -> PathBuf{
+    if path.is_dir() && (path.file_name() == Some(OsStr::new("data"))){
+        return path.to_path_buf();
+    }
+    else if path.file_name() != Some(OsStr::new("data")){
+        let mut acc_path = path.to_str().unwrap().to_string();
+        acc_path.push_str("/data");
+        let acc_path = Path::new(acc_path.as_str());
+        create_dir(acc_path).unwrap(); 
+        return acc_path.to_path_buf();  
+    }
+    else {
+        create_dir(path); 
+        return path.to_path_buf(); 
+    }
 }
 
 fn create_questions_json(path: &Path) -> (){
     if path.file_name() == Some(OsStr::new("questions.json")) && path.exists(){
         return; 
     }
+    //redundant check, I won't be passing in the full path; consider removing
     else if path.file_name() == Some(OsStr::new("questions.json")){
         File::create_new(path); 
     }
@@ -30,7 +48,7 @@ fn create_questions_json(path: &Path) -> (){
 }
 
 fn create_accountability_csv(path: &Path) -> (){
-    check_accountability_exists(path);
+    check_data_exists(path);
     let mut csv_path = path.to_str().unwrap().to_string();
     csv_path.push_str("/accountability_logs.csv");
     let csv_path = Path::new(csv_path.as_str()); 
@@ -50,18 +68,4 @@ fn write_questions() -> (){
     
 }
 
-fn check_accountability_exists(path: &Path) -> (){
-    if path.is_dir() && (path.file_name() == Some(OsStr::new("accountability"))){
-        return; 
-    }
-    else if path.file_name() != Some(OsStr::new("accountability")){
-        let mut acc_path = path.to_str().unwrap().to_string();
-        acc_path.push_str("/accountability");
-        let acc_path = Path::new(acc_path.as_str());
-        create_dir(acc_path).unwrap(); 
-    }
-    else {
-        create_dir(path); 
-    }
-}
 
